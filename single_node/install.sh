c2491046f4f045e9fd95dea08ceb04c655ca5074
Shrey <shrey.dabhi23@gmail.com>
2020-08-31 00:42:36 +0530

    add switch for environments

--- single_node/install.sh
+++ single_node/install.sh
@@ -1,14 +1,18 @@
 C='\033[0;32m'
 NC='\033[0m'
+mode=$1
 
-echo "${ORANGE}===================> install necessary tools <===================${NC}"
+echo "\033[0;36m$mode mode\033[0m"
 
 echo "${C}===================> install necessary tools <===================${NC}"
 apt-get -qq update
 apt-get -qq install -y ssh pdsh wget > /dev/null
 
 echo "${C}===================> retrieve and unpack hadoop <===================${NC}"
-# wget https://downloads.apache.org/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz
+if [ $mode = "prod" ]
+then
+    wget https://downloads.apache.org/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
+fi
 tar xzf hadoop-3.3.0.tar.gz
 rm -rf hadoop-3.3.0.tar.gz
 
